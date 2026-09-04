16d907b451f2a51eb672e6578d78a905f5d8d5d8
giusmili <giusmili67@gmail.com>
2026-09-04 10:35:40 +0200

    team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -29,7 +29,7 @@ const team = [
   },
    {
     name: "Diae Bootia El Oumani",
-    role: "Juriste & RH",
+    role: "Résponsable juridique de direction",
     image: "/asset/diae.png"
   },
     {
